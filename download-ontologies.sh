60df36f77d036363ad565d445181bd32bf70404a
Daniele Santini <danysan95@gmail.com>
2023-07-13 19:16:15 +0200

    Add wikidata rdf queries + update download script

--- download-ontologies.sh
+++ download-ontologies.sh
@@ -12,3 +12,6 @@ do
         sleep 1
     done
 done
+
+curl https://query.wikidata.org/sparql --data-urlencode 'query@wikidata/musicClassesShort.rq' --header 'Accept: application/rdf+xml' --output "wikidata/musicClassesShort.rdf" --verbose --location
+curl https://query.wikidata.org/sparql --data-urlencode 'query@wikidata/musicClassesFull_PARTIAL.rq' --header 'Accept: application/rdf+xml' --output "wikidata/musicClassesFull_PARTIAL.rdf" --verbose --location
