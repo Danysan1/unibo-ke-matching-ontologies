2ebb64150d60107da0cbb22e9e6f0cb7cf72cada
Daniele Santini <danysan95@gmail.com>
2023-07-16 15:53:31 +0200

    Fix wikidata Download script

---
+++ TODO.md
@@ -0,0 +1,4 @@
+* Risolvere problema query Wikidata che non trova alcune superclassi
+* Finire i known mapping Polifonia-Wikidata in Core
+* testare i moduli di Polifonia con Protege/BERTMap
+* capire perchè BERTMap esplode con Wikidata
--- wikidata/download-ontologies.py
+++ wikidata/download-ontologies.py
@@ -2,6 +2,7 @@ import requests
 import os
 import sys
 import xml.etree.ElementTree as ET
+
 script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
 url = 'https://query.wikidata.org/sparql'
 
@@ -19,21 +20,22 @@ for baseName in ['musicClasses', 'musicClassesFull_PARTIAL']:
     rdfFilePath = os.path.join(script_directory, baseName + '.rdf')
     owlFilePath = os.path.join(script_directory, baseName + '.owl')
 
-    # print(f'Downloading {baseName}...')
-    # r = requests.get(
-    #     url,
-    #     allow_redirects=True,
-    #     headers={'Accept': 'application/rdf+xml', 'User-Agent': 'unibo-ke-matching-ontologies/0.0.1'},
-    #     data={'query': open(sparqlFilePath, 'r').read()}
-    # )
-    # open(rdfFilePath, 'wb').write(r.content)
+    print(f'Downloading {baseName}...')
+    sparqlQuery = open(sparqlFilePath, 'r').read()
+    r = requests.post(
+        url,
+        allow_redirects=True,
+        headers={'Accept': 'application/rdf+xml', 'User-Agent': 'unibo-ke-matching-ontologies/0.0.1'},
+        data={'query': sparqlQuery}
+    )
+    open(rdfFilePath, 'wb').write(r.content)
 
     print(f'Updating {baseName} to OWL...')
     register_all_namespaces(rdfFilePath)
     tree = ET.parse(rdfFilePath)
     root = tree.getroot()
-    root.append(ET.Element(
-        '{http://www.w3.org/2002/07/owl#}Ontology',
-        {'{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about': 'http://www.wikidata.org/'}
-    ))
+    root.append(ET.Element('{http://www.w3.org/2002/07/owl#}Ontology', {
+        '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about': 'http://www.wikidata.org/',
+        '{http://purl.org/dc/elements/1.1/}description': 'OWL export of Wikidata classes related to music'
+    }))
     tree.write(owlFilePath, default_namespace="http://www.wikidata.org/")
