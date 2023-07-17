69aa83ea2779a06e443ff50ce040b05b944c62b0
Daniele Santini <danysan95@gmail.com>
2023-07-17 22:41:20 +0200

    Update Wikidata owl queries

--- wikidata/download-ontologies.py
+++ wikidata/download-ontologies.py
@@ -3,39 +3,59 @@ import os
 import sys
 import xml.etree.ElementTree as ET
 
-script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
-url = 'https://query.wikidata.org/sparql'
 
-def register_all_namespaces(filename):
+def register_xml_namespaces_from_file(file_name):
     """
     From https://stackoverflow.com/a/54491129/2347196
     """
-    namespaces = dict([node for _, node in ET.iterparse(filename, events=['start-ns'])])
+    namespaces = dict(
+        [node for _, node in ET.iterparse(file_name, events=['start-ns'])]
+    )
     for ns in namespaces:
         ET.register_namespace(ns, namespaces[ns])
 
-print('========== Downloading ontologies from Wikidata ==========')
-for baseName in ['musicClasses', 'musicClassesFull_PARTIAL']:
-    sparqlFilePath = os.path.join(script_directory, baseName + '.rq')
-    rdfFilePath = os.path.join(script_directory, baseName + '.rdf')
-    owlFilePath = os.path.join(script_directory, baseName + '.owl')
 
-    print(f'Downloading {baseName}...')
+def download_rdf_from_sparql(base_path):
+    sparqlFilePath = base_path + '.rq'
+    rdf_file_path = base_path + '.rdf'
+
+    print(f'Downloading {rdf_file_path}...')
     sparqlQuery = open(sparqlFilePath, 'r').read()
     r = requests.post(
-        url,
+        'https://query.wikidata.org/sparql',
         allow_redirects=True,
-        headers={'Accept': 'application/rdf+xml', 'User-Agent': 'unibo-ke-matching-ontologies/0.0.1'},
+        headers={'Accept': 'application/rdf+xml'},
         data={'query': sparqlQuery}
     )
-    open(rdfFilePath, 'wb').write(r.content)
+    open(rdf_file_path, 'wb').write(r.content)
 
-    print(f'Updating {baseName} to OWL...')
-    register_all_namespaces(rdfFilePath)
-    tree = ET.parse(rdfFilePath)
-    root = tree.getroot()
-    root.append(ET.Element('{http://www.w3.org/2002/07/owl#}Ontology', {
-        '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about': 'http://www.wikidata.org/',
-        '{http://purl.org/dc/elements/1.1/}description': 'OWL export of Wikidata classes related to music'
-    }))
-    tree.write(owlFilePath, default_namespace="http://www.wikidata.org/")
+
+print('========== Downloading musicClasses ontology from Wikidata ==========')
+script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+class_base_path = os.path.join(script_directory, 'musicClasses')
+download_rdf_from_sparql(class_base_path)
+
+print(f'Adding owl:ontology to ontology...')
+register_xml_namespaces_from_file(class_base_path+'.rdf')
+output_xml_tree = ET.parse(class_base_path+'.rdf')
+output_xml_root = output_xml_tree.getroot()
+output_xml_root.insert(0, ET.Element('{http://www.w3.org/2002/07/owl#}Ontology', {
+    '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about': 'http://www.wikidata.org/',
+    '{http://purl.org/dc/elements/1.1/}description': 'OWL export of Wikidata classes related to music'
+}))
+
+for base_name in ['musicDataProperties', 'musicObjectProperties']:
+    base_path = os.path.join(script_directory, base_name)
+    print(f'Downloading {base_name} ontology from Wikidata')
+    download_rdf_from_sparql(base_path)
+
+    print(f'Merging {base_name} ontology into ontology')
+    property_xml_tree = ET.parse(base_path+'.rdf')
+    property_xml_root = property_xml_tree.getroot()
+    for property_element in property_xml_root:
+        output_xml_root.append(property_element)
+
+output_xml_tree.write(
+    os.path.join(script_directory, 'ontology', 'music.owl'),
+    default_namespace="http://www.wikidata.org/"
+)
