32f57740459da391e3d5e8059903d9d891fad2a6
Daniele Santini <danysan95@gmail.com>
2023-07-19 19:28:59 +0200

    Fix algorithm-pattern reference

--- polifonia/download-ontologies.sh
+++ polifonia/download-ontologies.sh
@@ -7,7 +7,7 @@ for row in $(cat "$thisFolder/namespaces.csv")
 do
     moduleName=$(echo "$row" | cut -d ',' -f 1)
     moduleURI=$(echo "$row" | cut -d ',' -f 2)
-    fileName="$thisFolder/ontology/$moduleName.owl"
+    fileName="$thisFolder/ontology/$moduleName"
     echo "Downloading $fileName from $moduleURI"
     curl "$moduleURI" -H 'Accept: application/rdf+xml' -o "$fileName" -L
     sleep 1
