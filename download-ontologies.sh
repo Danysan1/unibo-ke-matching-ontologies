2d89963de109dc9000daa9175d16321e7f83d55d
Daniele Santini <danysan95@gmail.com>
2023-09-10 22:35:34 +0200

    add manual matches to .nq + improve merge script

--- download-ontologies.sh
+++ download-ontologies.sh
@@ -8,3 +8,6 @@ python ./wikidata/download-ontologies.py
 
 python ./get_known_mappings.py
 sort --ignore-case -o ./known_mappings.nq ./known_mappings.nq
+
+cd wikidata
+python ./merge_true_with_bertmap.py
--- wikidata/merge_true_with_bertmap.py
+++ wikidata/merge_true_with_bertmap.py
@@ -2,17 +2,19 @@ import pandas as pd
 import os
 from sklearn.metrics import classification_report
 
-all_tsv_file_path = os.path.join(os.getcwd(), 'polifonia_wikidata_filtered.tsv')
+source_tsv_file_path = os.path.join(os.getcwd(), 'polifonia_wikidata.tsv')
+all_tsv_file_path = os.path.join(os.getcwd(), 'all.tsv')
 bertmap_tsv_file_path = os.path.join(os.getcwd(), 'bertmap_repaired_mappings.tsv')
 out_tsv_file_path = os.path.join(os.getcwd(), 'all_with_bertmap.tsv')
 
-all_df = pd.read_csv(all_tsv_file_path, sep='\t', names=['PolifoniaClass','TrueWikidataClass'])
-bertmap_df = pd.read_csv(bertmap_tsv_file_path, sep='\t', names=['PolifoniaClass','BertWikidataClass','Score'])
+all_df = pd.read_csv(source_tsv_file_path, sep='\t', header=0, names=['PolifoniaClass','TrueWikidataClass'])
+bertmap_df = pd.read_csv(bertmap_tsv_file_path, sep='\t', header=None, names=['PolifoniaClass','BertWikidataClass','Score'])
+
+all_df = all_df[all_df.notna().all(1)]
+all_df.to_csv(all_tsv_file_path, sep='\t', index=False, header=True)
 
 output_df = pd.merge(all_df, bertmap_df, how='outer', on=['PolifoniaClass'])
 output_df = output_df[['PolifoniaClass','TrueWikidataClass','BertWikidataClass']]
 
-if(os.path.exists(out_tsv_file_path)):
-    os.unlink(out_tsv_file_path)
 output_df.to_csv(out_tsv_file_path, sep='\t', index=False, header=True)
 
