6e8bf09098d0649c7378a60752d5f523f5204b37
Daniele Santini <danysan95@gmail.com>
2023-09-23 12:27:59 +0200

    Complete all_with_bertmap + report.txt

--- wikidata/evaultate_all_with_bertmap.py
+++ wikidata/evaultate_all_with_bertmap.py
@@ -1,8 +1,16 @@
 import pandas as pd
 from sklearn.metrics import classification_report
 
-output_df = pd.read_csv('all_with_bertmap.tsv', sep='\t', names=['PolifoniaClass','TrueWikidataClass','BertWikidataClass'])
+output_df = pd.read_csv(
+    'all_with_bertmap.tsv',
+    sep='\t',
+    skiprows=1,
+    names=['PolifoniaClass','TrueWikidataClass','BertWikidataClass']
+)
 
 y_true = output_df['TrueWikidataClass'].tolist()
 y_pred = output_df['BertWikidataClass'].tolist()
-print(classification_report(y_true, y_pred, zero_division=0))
+report = classification_report(y_true, y_pred, zero_division=0)
+with open('report.txt', 'w') as f:
+    f.write(report)
+print(report)
