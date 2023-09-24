aafc4ae44cac1d92fc0c9829b22647a207766dee
Daniele Santini <daniele.santini-external@vivaticket.com>
2023-09-24 23:59:08 +0200

    Re-run model
    Nel notebook per qualche motivo si era perso l'output della fase di training (fine tuning del modello).
    
    Ho:
    * leggermente migliorato la query di download delle classi da wikidata (passando da 62k triple a quasi 64k)
    * modificato nello yaml il numero di epoche da 3 a 4
    * riportato tutte i mapping manuali da all_with_bertmap.tsv a known_mapopings.nq
    * fatto girare il notebook di nuovo

--- wikidata/merge_true_with_bertmap.py
+++ wikidata/merge_true_with_bertmap.py
@@ -3,7 +3,7 @@ import os
 from sklearn.metrics import classification_report
 
 source_tsv_file_path = os.path.join(os.getcwd(), 'polifonia_wikidata.tsv')
-all_tsv_file_path = os.path.join(os.getcwd(), 'all.tsv')
+with_mapping_tsv_file_path = os.path.join(os.getcwd(), 'with_mapping.tsv')
 bertmap_tsv_file_path = os.path.join(os.getcwd(), 'bertmap_repaired_mappings.tsv')
 out_tsv_file_path = os.path.join(os.getcwd(), 'all_with_bertmap.tsv')
 
@@ -11,7 +11,7 @@ all_df = pd.read_csv(source_tsv_file_path, sep='\t', header=0, names=['Polifonia
 bertmap_df = pd.read_csv(bertmap_tsv_file_path, sep='\t', header=None, names=['PolifoniaClass','BertWikidataClass','Score'])
 
 all_df = all_df[all_df.notna().all(1)]
-all_df.to_csv(all_tsv_file_path, sep='\t', index=False, header=True)
+all_df.to_csv(with_mapping_tsv_file_path, sep='\t', index=False, header=True)
 
 output_df = pd.merge(all_df, bertmap_df, how='outer', on=['PolifoniaClass'])
 output_df = output_df[['PolifoniaClass','TrueWikidataClass','BertWikidataClass']]
