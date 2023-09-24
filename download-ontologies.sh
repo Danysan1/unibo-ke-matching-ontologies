495c690e730913f0c144e6de1203eff2771b4346
Daniele Santini <daniele.santini-external@vivaticket.com>
2023-09-24 20:04:55 +0200

    Add download-ontologies.sh to flow.svg

--- download-ontologies.sh
+++ download-ontologies.sh
@@ -8,6 +8,3 @@ python ./wikidata/download-ontologies.py
 
 python ./get_known_mappings.py
 sort --ignore-case -o ./known_mappings.nq ./known_mappings.nq
-
-cd wikidata
-python ./merge_true_with_bertmap.py
